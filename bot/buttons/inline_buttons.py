5032b55d2339491c17010c96ed3918b48d996465
ishigami <sejuroakashi137@gmail.com>
2025-09-22 08:19:46 -0700

    крутота

--- bot/buttons/inline_buttons.py
+++ bot/buttons/inline_buttons.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 
 
 menu = [
-    [InlineKeyboardButton(text='Mini App', url='https://habr.com')]
+    [InlineKeyboardButton(text='Mini App', web_app=WebAppInfo())]
 ]
 
 inline_keybords = InlineKeyboardMarkup(inline_keyboard=menu)
